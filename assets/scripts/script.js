448ad925dbea9448986fcc1a7e32ca7d6168447a
Prestgg <prestgg56@gmail.com>
2023-11-26 13:21:14 +0400

    Artificial-Intelligence

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+
+
+var loading = document.getElementById("loading");
+setTimeout(()=>{
+    loading.style.transition = "550ms all";
+    loading.style.opacity = 0;
+    loading.style.visibility = "hidden";
+},4000)
